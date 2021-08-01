f65032b7bb8efe0608e1cdd6f817332cd76ea20d
lamia <medenglam@windowslive.com>
2021-08-01 19:29:38 +0300

    add filters

--- job/models.py
+++ job/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
 JOB_TYPE={
@@ -8,7 +10,10 @@ JOB_TYPE={
 def image_upload(instance,filename):
     imagename,extension= filename.split(".")
     return "jobs/%s.%s"%(instance.id,extension)
+
+
 class Job(models.Model):
+    owner=models.ForeignKey(User,related_name='job_owner',on_delete=models.CASCADE)
     title= models.CharField(max_length=100)
     #location=models.CharField(max_length=100)
     job_type= models.CharField(max_length=15,choices=JOB_TYPE,default='Full Time')
@@ -19,12 +24,29 @@ class Job(models.Model):
     experience=models.IntegerField(default=1)
     category=models.ForeignKey('Category',on_delete=models.CASCADE)
     image=models.ImageField(upload_to=image_upload)
-
+    slug=models.SlugField(blank=True,null=True)
+    
+    def save(self,*args,**kwargs):
+        self.slug=slugify(self.title)
+        super(Job,self).save(*args,**kwargs)
     def __str__(self):
         return self.title
 
 class Category(models.Model):
     name=models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+
+class Apply(models.Model):
+    job=models.ForeignKey(Job,related_name='apply_job',on_delete=models.CASCADE)
+    name=models.CharField(max_length=50)
+    email=models.EmailField(max_length=100)
+    website=models.URLField()
+    cv=models.FileField(upload_to='apply/')
+    cover_letter=models.TextField(max_length=500)
+    created_at=models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
--- project/settings.py
+++ project/settings.py
@@ -31,18 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'account',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
     # my apps
     'blog',
     'job',
     'home',
     'contact',
-    'account',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
